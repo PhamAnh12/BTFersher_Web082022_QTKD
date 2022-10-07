e8e36e3ce0901118eebefaab836afca2f457408d
Phamanh12 <phamcongtuananh99@gmail.com>
2022-10-08 00:29:52 +0700

    Feat: nop bai tap QTKD

---
+++ Misa.Web082022.QTKD.Multilayer/Misa.Web082022.QTKD.Multilayer.Common/Enums/QTKDErrorCode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misa.Web082022.QTKD.Multilayer.Common.Enums
+{
+    /// <summary>
+    /// Các mã lỗi
+    /// </summary>
+    public enum QTKDErrorCode
+    {
+        /// <summary>
+        /// Lỗi do Exception
+        /// </summary>
+        Exception = 1,
+
+        /// <summary>
+        /// Lỗi khi lây, thêm ,sửa,xóa vào data không thành công
+        /// </summary>
+        ResultDatabaseFailed = 2,
+
+        /// <summary>
+        /// Lỗi do trùng mã nhân viên
+        /// </summary>
+        DuplicateCode = 3,
+
+        /// <summary>
+        /// Lỗi do các trường để trống
+        /// </summary>
+        InputValidation = 4
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+frontend
+Project setup
+ npm install
+ Compiles and hot-reloads for development
+ npm run serve
+ Data 
+ Tạo data có tên `misa.web08.qtkd.pctuananh`
+ Restore file Data có Trong File Data
